a37f1d25fd2eceb3e001235377ab133eee41adf4
Kurdiamina3 <kurdiamina3@gmail.com>
2021-11-04 17:54:41 -0400

    can delete note

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
@@ -10,12 +9,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(express.static('public'));
-app.use('/api',apiRoutes);
-app.use('/',htmlRoutes);
+app.use(express.static(__dirname));
+
+require('./routes/routes')(app);
 
 
-app.listen(PORT, () => {
+app.listen(PORT, function() {
   console.log (`API server now on port ${PORT}`);
 });
 
